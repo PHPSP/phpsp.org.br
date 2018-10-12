89837fb9fe83add602b96f1a6dd586138a6a347a
João Paulo V Martins <joao.paulo@smartbox.com>
2018-10-12 15:06:57 +0100

    Adiciona página para erro 404

---
+++ source/404.blade.php
@@ -0,0 +1,18 @@
+---
+title: Memory Stack
+extends: _layouts.master
+section: content
+permalink: 404.html
+---
+@extends('_layouts.master')
+
+@section('body')
+    <section class="section">
+        <h1 class="title is-1">404 - Página não encontrada</h1>
+        <div class="container">
+            <h1 class="title">Deu ruim...</h1>
+            <h2 class="subtitle">O conteúdo que você buscou não foi encontrado.</h2>
+            <p>Ele pode ter sido movido ou, o mais provável, abdusido por aliens!</p>
+        </div>
+    </section>
+@endsection
