1438fa773cb6754c748895158e88afe60ef57671
João Paulo V Martins <joao.paulo@smartbox.com>
2018-10-15 16:14:08 +0100

    Adiciona o favicon!

--- source/_partials/layout/head.blade.php
+++ source/_partials/layout/head.blade.php
@@ -2,5 +2,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
 <title>PHPSP | {{$page->title}}</title>
+<link rel="shortcut icon" href="{{ $page->asset_prefix }}/assets/images/phpsp/favicon.ico" />
 <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
 <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
