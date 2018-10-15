ced370d8565fa4c7ba2772fdffa30100f21c0091
João Paulo V Martins <joao.paulo@smartbox.com>
2018-10-15 19:46:58 +0100

    Move os outros tipos de conteúdos da index para parciais

---
+++ source/_partials/content/notices/list.blade.php
@@ -0,0 +1,47 @@
+<div class="card">
+    <div class="card-content">
+        <p class="subtitle">
+            Salve pessoal! Além do PHPSP+Pub, encontro mensal do grupo, temos mais eventos sendo
+            construídos com a ajuda da comunidade e dos parceiros. Quer nos ajudar e fazer parte do
+            grupo? Acesse a seção <a href="/como-contribuir/">como contribuir</a> e ajude a evangelizar
+            o PHPSP!
+        </p>
+    </div>
+    <footer class="card-footer">
+        <p class="card-footer-item">
+          <span>
+            <a href="{{ $page->links->twitter->url }}">Twitter</a>
+          </span>
+        </p>
+        <p class="card-footer-item">
+          <span>
+            <a href="{{ $page->links->facebook->url }}">Facebook</a>
+          </span>
+        </p>
+        <p class="card-footer-item">
+          <span>
+            <a href="{{ $page->links->slack->url }}">Slack</a>
+          </span>
+        </p>
+    </footer>
+</div>
+<hr>
+<div class="card">
+    <div class="card-content">
+        <p class="subtitle">
+            Ajude-nos a divulgar mais e mais conteúdos interessantes do mundo PHP, colabore com conteúdos e com projetos!
+        </p>
+    </div>
+    <footer class="card-footer">
+        <p class="card-footer-item">
+          <span>
+            <a href="/como-contribuir">Como Contribuir?</a>
+          </span>
+        </p>
+        <p class="card-footer-item">
+          <span>
+            <a href="{{ $page->links->github->url }}">Github</a>
+          </span>
+        </p>
+    </footer>
+</div>
--- source/index.blade.php
+++ source/index.blade.php
@@ -22,8 +22,7 @@ pagination:
                 <p>
                     A comunidade se reune frequentemente. Veja qual encontro vai aconcer ao seu redor!
                 </p>
-                <hr>
-                <meetup-events url="{{ $page->meetup['baseUrl'] }}"></meetup-events>
+                @include('_partials.content.events.list')
             </div>
             <div class="column">
                 <h2 class="title is-2">
@@ -35,33 +34,7 @@ pagination:
                 <h2 class="title is-2">
                     <a class="has-text-grey" href="{{ $page->getUrl() }}avisos">Avisos</a>
                 </h2>
-                <div class="card">
-                    <div class="card-content">
-                        <p class="subtitle">
-                            Salve pessoal! Além do PHPSP+Pub, encontro mensal do grupo, temos mais eventos sendo
-                            construídos com a ajuda da comunidade e dos parceiros. Quer nos ajudar e fazer parte do
-                            grupo? Acesse a seção <a href="/como-contribuir/">como contribuir</a> e ajude a evangelizar
-                            o PHPSP!
-                        </p>
-                    </div>
-                    <footer class="card-footer">
-                        <p class="card-footer-item">
-                          <span>
-                            <a href="https://twitter.com/phpsp">Twitter</a>
-                          </span>
-                        </p>
-                        <p class="card-footer-item">
-                          <span>
-                            <a href="https://facebook.com/sao.paulo.elephants">Facebook</a>
-                          </span>
-                        </p>
-                        <p class="card-footer-item">
-                          <span>
-                            <a href="https://bit.ly/vemproslackphpsp">Slack</a>
-                          </span>
-                        </p>
-                    </footer>
-                </div>
+                @include('_partials.content.notices.list')
             </div>
         </div>
     </section>
