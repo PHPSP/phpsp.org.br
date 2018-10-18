38cc93ff2f518377ed01200ec22ceb95984978a4
Joao Paulo Vendramini Martins <jp.joao@gmail.com>
2018-10-18 08:10:00 +0100

    Adiciona código para o Google Analytics;

--- source/_partials/layout/head.blade.php
+++ source/_partials/layout/head.blade.php
@@ -3,6 +3,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>PHPSP | {{$page->title}}</title>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id={{ $page->google_analytics->id }}"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        
+        gtag('config', '{{ $page->google_analytics->id }}');
+    </script>
+
     <link rel="shortcut icon" href="{{ $page->asset_prefix }}/assets/images/phpsp/favicon.ico"/>
     <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
