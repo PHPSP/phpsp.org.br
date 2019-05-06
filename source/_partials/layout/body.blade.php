3ebae0df2fe6b6255d2eaf517751df87fc2773f5
Nickolas Da Silva <nickolas.silva@researchgate.net>
2019-05-06 15:18:43 +0200

    Applying changes to all .container elements

--- source/_partials/layout/body.blade.php
+++ source/_partials/layout/body.blade.php
@@ -1,6 +1,6 @@
 <body>
 <section id="app" class="section">
-    <div class="container article">
+    <div class="container">
         @include('_partials.layout.navbar')
         @include('_partials.layout.breadcrumbs')
         @yield('body')
