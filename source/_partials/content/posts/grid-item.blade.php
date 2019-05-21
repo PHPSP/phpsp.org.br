922b558020b2c0b107d8614ab877f353c6949069
José Filho <jose-silvafilho@hotmail.com>
2019-05-21 01:14:57 -0300

    modelo inicial de melhoria na listagem de artigos

--- source/_partials/content/posts/grid-item.blade.php
+++ source/_partials/content/posts/grid-item.blade.php
@@ -1,11 +1,22 @@
-<article class="tile is-child box">
-    <p class="title">
-        <a href="{{ $post->getPath() }}">{{ $post->title }}</a>
-    </p>
-    <p class="subtitle">
-        <small>por</small>
-        <strong> {{ $post->author }}</strong>
-        <small> em</small>
-        <small> {{ date('d\/m\/Y', $post->createdAt) }}</small>
-    </p>
+<article class="card">
+    <a href="{{ $post->getPath() }}">
+        <div class="card-content has-text-black	">
+            <div class="media">
+                <div class="media-left">
+                    <figure class="image is-64x64">
+                        <?php $emailHash = md5(strtolower(trim($post->authorEmail))); ?>
+                        <img class="is-rounded" src="https://www.gravatar.com/avatar/{{$emailHash}}?s=64&d=retro&r=g" alt="Gravatar de {{ $post->author }}">
+                    </figure>
+                </div>
+                <div class="media-content">
+                    <p class="title is-5">{{ $post->author }}</p>
+                </div>
+            </div>
+            <div class="content">
+                <br>
+                <time class="level-right is-italic is-size-6" datetime="{{ date('d-m-Y', $post->createdAt) }}">{{ date('d\/m\/Y', $post->createdAt) }}</time>
+                <p class="is-centered has-text-weight-semibold is-size-5">{{ $post->title }}</p>
+            </div>
+        </div>
+    </a>
 </article>
