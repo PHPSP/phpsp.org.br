68d6c2070acd42a3d4b8273fdc4e497d34ec3efd
José Filho <jose-silvafilho@hotmail.com>
2019-05-21 21:37:43 -0300

    melhorando estilizaçao e responsividade da pagina artigos

--- source/_partials/content/posts/grid-item.blade.php
+++ source/_partials/content/posts/grid-item.blade.php
@@ -1,22 +1,23 @@
-<article class="card">
-    <a href="{{ $post->getPath() }}">
-        <div class="card-content has-text-black	">
-            <div class="media">
-                <div class="media-left">
-                    <figure class="image is-64x64">
-                        <?php $emailHash = md5(strtolower(trim($post->authorEmail))); ?>
-                        <img class="is-rounded" src="https://www.gravatar.com/avatar/{{$emailHash}}?s=64&d=retro&r=g" alt="Gravatar de {{ $post->author }}">
-                    </figure>
-                </div>
-                <div class="media-content">
-                    <p class="title is-5">{{ $post->author }}</p>
-                </div>
-            </div>
-            <div class="content">
-                <br>
-                <time class="level-right is-italic is-size-6" datetime="{{ date('d-m-Y', $post->createdAt) }}">{{ date('d\/m\/Y', $post->createdAt) }}</time>
-                <p class="is-centered has-text-weight-semibold is-size-5">{{ $post->title }}</p>
-            </div>
+<article class="tile is-child card">
+    <a class="article-item" href="{{ $post->getPath() }}">
+
+        <header class="card-header-title is-centered">
+            <figure class="image is-64x64">
+                <?php $emailHash = md5(strtolower(trim($post->authorEmail))); ?>
+                <img class="is-rounded" src="https://www.gravatar.com/avatar/{{$emailHash}}?s=64&d=retro&r=g" alt="Gravatar de {{ $post->author }}">
+
+            </figure>
+        </header>
+        <div class="card-content is-paddingless">
+            <p class="title is-size-6 has-text-centered	">{{$post->author}}</p>
+            <p class="subtitle is-size-7 has-text-centered">
+                em <time class="is-size-8" datetime="{{ date('d-m-Y', $post->createdAt) }}">{{ date('d\/m\/Y', $post->createdAt) }}</time>
+            </p>
+        </div>
+        <div class="card-content">
+            <p class="is-uppercase is-size-6 has-text-weight-semibold has-text-centered">
+                {{ $post->title }}
+            </p>
         </div>
     </a>
 </article>
