c5f65f0d8a094d474abcc03f9cffd79ed6552ee3
nawarian <nickolas@phpsp.org.br>
2019-07-04 20:36:12 +0200

    Renomeia sitemap antigo e adiciona novo sitemap para páginas de conteúdo
    - Renomeia sitemap.xml para sitemap-posts.xml
    - Adiciona sitemap-contents.xml

--- app/Site/EventHandlers/AfterBuild/GenerateSitemap.php
+++ app/Site/EventHandlers/AfterBuild/GenerateSitemap.php
@@ -2,23 +2,26 @@
 
 namespace Phpsp\Site\EventHandlers\AfterBuild;
 
-use Phpsp\Site\EventHandlers\JigsawHandlerInterface;
-use PODEntender\SitemapGenerator\Adapter\Jigsaw\JigsawAdapter;
 use TightenCo\Jigsaw\Jigsaw;
+use PODEntender\SitemapGenerator\Adapter\Jigsaw\JigsawAdapter;
+use Phpsp\Site\EventHandlers\JigsawHandlerInterface;
 
 class GenerateSitemap implements JigsawHandlerInterface
 {
-    const SITEMAP_FILENAME = 'sitemap.xml';
+    const SITEMAP_POSTS_FILENAME = 'sitemap-posts.xml';
+    const SITEMAP_CONTENTS_FILENAME = 'sitemap-contents.xml';
 
     public function handle(Jigsaw $jigsaw): void
     {
-        $output = $jigsaw->getDestinationPath() . DIRECTORY_SEPARATOR . self::SITEMAP_FILENAME;
+        $output = $jigsaw->getDestinationPath() . DIRECTORY_SEPARATOR;
 
         /** @var JigsawAdapter $generator */
         $generator = $jigsaw->app->make(JigsawAdapter::class);
 
-        $xmlSitemap = $generator->fromCollection($jigsaw->getCollection('posts'));
+        $postsSitemap = $generator->fromCollection($jigsaw->getCollection('posts'));
+        file_put_contents($output . self::SITEMAP_POSTS_FILENAME, $postsSitemap->saveXML());
 
-        file_put_contents($output, $xmlSitemap->saveXML());
+        $contentsSitemap = $generator->fromCollection($jigsaw->getCollection('contents'));
+        file_put_contents($output . self::SITEMAP_CONTENTS_FILENAME, $contentsSitemap->saveXML());
     }
 }
