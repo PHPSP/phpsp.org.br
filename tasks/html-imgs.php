c4ead600be9ad5a81d9662bc1e8fc5cf46f66578
leocavalcante <lc@leocavalcante.com>
2020-10-31 11:09:03 -0300

    refactor: md to html images

---
+++ tasks/html-imgs.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+[, $filename] = $argv;
+
+$handle = fopen($filename, 'rwb');
+$rewrite = '';
+
+while ($row = fgets($handle)) {
+    if (preg_match('/^!\[(.*)\]\((.*)\)/', $row, $matches)) {
+        [, $alt, $src] = $matches;
+        $row = "<p align=\"center\"><img alt=\"$alt\" src=\"$src\"></p>";
+    }
+
+    $rewrite .= $row;
+}
+
+file_put_contents($filename, $rewrite);
